e86d8e82d6540a0bab6c6655806f3b87d83e67cc
Yoni <yoniyoni920@gmail.com>
2025-05-27 10:12:58 +0300

    preper for deployment

--- client_smart_tutor/vite.config.js
+++ client_smart_tutor/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    open:true,
-  proxy:{
-    '/api': {//proxi api request to server
-      target: 'http://localhost:3000',
-    changeOrigin: true,
+  base: '/WebProjectTeam5React/', // 👈 Add this line
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
-  }
-  }
 })
